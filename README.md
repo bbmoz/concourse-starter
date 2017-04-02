0317c5f53e7dff42c49e3823e551cc841e6f895d
Joe Lee <blueberrymozart@gmail.com>
2017-04-02 17:50:48 -0500

    Quickly spin up concourse.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Concourse Starter
+
+> Quickly spin up your own concourse ci.
+
+Run `./run.sh` to see usages or simply run `./run.sh a`.
---
+++ run.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+set -e
+
+tunnel () {
+  set -x
+  ./ngrok-env/run.sh start
+  direnv allow
+}
+
+keys () {
+  set -x
+  mkdir -p keys/web keys/worker
+  ssh-keygen -t rsa -f ./keys/web/tsa_host_key -N ''
+  ssh-keygen -t rsa -f ./keys/web/session_signing_key -N ''
+  ssh-keygen -t rsa -f ./keys/worker/worker_key -N ''
+  cp ./keys/worker/worker_key.pub ./keys/web/authorized_worker_keys
+  cp ./keys/web/tsa_host_key.pub ./keys/worker
+}
+
+start () {
+  set -x
+  docker-compose up -d
+  docker-compose ps
+}
+
+all () {
+  set -x
+  tunnel
+  keys
+  start
+}
+
+usage () {
+cat <<EOF
+  Usage:
+    tunnel|t    start tunnel
+    keys|k      generate keys
+    start|s     start concourse
+    all|a       start tunnel, generate keys, and start concourse
+    *           see usage
+EOF
+}
+
+case $1 in
+  tunnel|t)   tunnel    ;;
+  keys|k)     keys      ;;
+  start|s)    start     ;;
+  all|a)      all       ;;
+  *)          usage     ;;
+esac
